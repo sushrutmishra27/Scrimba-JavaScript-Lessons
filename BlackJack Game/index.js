df4db364f57ef2f62e179e1799412c954da8a157
Sushrut Mishra <41235363+sushrutmishra27@users.noreply.github.com>
2022-10-10 22:36:32 +0530

    Update index.js

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -7,6 +7,14 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el") 
 
+let player = {
+    name: "Sushrut",
+    chips: 150
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random() + 13) + 1
     if(randomNumber === 1) {
@@ -29,7 +37,7 @@ function startGame() {
 
 function renderGame() {
     cardsEl.textContent = "Cards: " 
-    for(let i=0 ; i<=cards.length; i++){
+    for(let i=0 ; i<cards.length; i++){
         cardsEl.textContent += cards[i] + " "
     }
     sumEl.textContent = "Sum: " + sum
@@ -47,8 +55,11 @@ function renderGame() {
 
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card) 
-    renderGame()
+    if(isAlive == true && hasBlackJack == false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card) 
+        renderGame()
+    }
+    
 }
