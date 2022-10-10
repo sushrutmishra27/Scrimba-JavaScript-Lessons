a705a56af7b8496d7cc8541a4c38ef178d2a9bf7
Sushrut Mishra <41235363+sushrutmishra27@users.noreply.github.com>
2022-10-10 14:57:29 +0530

    Update index.js

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,22 +1,37 @@
-let firstCard = 10
-let secondCard = 4
-// 1. Create a new array - cards - that contains firstCard and secondCard
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true 
+let isAlive = false 
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el") 
 
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random() + 13) + 1
+    if(randomNumber === 1) {
+        return 11
+    } else if(randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    } 
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
-    // 2. Refer to the cards array when rendering out the cards
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: " 
+    for(let i=0 ; i<=cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -32,7 +47,8 @@ function renderGame() {
 
 
 function newCard() {
-    let card = 6
+    let card = getRandomCard()
     sum += card
+    cards.push(card) 
     renderGame()
 }
