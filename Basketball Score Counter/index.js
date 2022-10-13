6d7ec2ba7ff8ed47e9d55bcc8595d11a92f96258
Sushrut Mishra <41235363+sushrutmishra27@users.noreply.github.com>
2022-10-13 13:51:40 +0530

    Update index.js

--- Basketball Score Counter/index.js
+++ Basketball Score Counter/index.js
@@ -1,6 +1,5 @@
 let homeScoreEl = document.getElementById("home-score");
 let guestScoreEl = document.getElementById("guest-score");
-
 let homeScoreBtnOne = document.getElementById("home-score-btn-1");
 let homeScoreBtnTwo = document.getElementById("home-score-btn-2");
 let homeScoreBtnThree = document.getElementById("home-score-btn-3");
